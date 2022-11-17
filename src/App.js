72502802a0f22117e54711aeb93424b4b9023fef
codedependant74 <brendale74@gmail.com>
2022-11-17 11:53:40 -0500

    updated card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ERCard from "./components/er-component";
 
 function App() {
   return (
     <div className="App">
+      <ERCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
