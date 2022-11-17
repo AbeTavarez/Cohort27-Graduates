8c83d93971ccb9f2f38597db2a4454f684871dbe
chrisleg19 <110962234+chrisleg19@users.noreply.github.com>
2022-11-17 11:41:22 -0500

    updated my card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import CLCard from './components/cl-components';
+
 function App() {
   return (
     <div className="App">
+      <CLCard/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
