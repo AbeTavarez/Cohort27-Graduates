ad17cdc0e62e3ab294a0d7ce3d9d79937a8878ce
Lachezar Shishmanov <lachezarshishmanov@Lachezars-MacBook-Pro.local>
2022-11-17 11:41:21 -0500

    git link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LSCard from './components/ls-components';
 
 function App() {
   return (
     <div className="App">
+      <LSCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/ls-components/index.js
+++ src/components/ls-components/index.js
@@ -1,12 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
+const LSCard = () => {
+    return (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/110977901?s=96&v=4" />
+          <Card.Body>
+            <Card.Title>Lachezar Shishmanov</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <a href='https://github.com/LachezarShishmanov' target='_blank'>
+          <Button variant="primary">GitHub</Button>
+        </a>
+          </Card.Body>
+        </Card>
+      );
+    };
 
-
-const LSCard = ()=> {
-   return(
-    <div>
-        <h1>Lachezar</h1>
-    </div>
-   ) 
-}
-
-export default LSCard
+export default LSCard;
