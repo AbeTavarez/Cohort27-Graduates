9cc3e052d350a64eb18a99fe873eeb2391bf62a9
Deja Mann <110996266+DejaMann@users.noreply.github.com>
2022-11-17 11:46:51 -0500

    added card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DMCard from "./components/dm-component"
 
 function App() {
   return (
     <div className="App">
+      <DMCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
