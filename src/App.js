717cd5abf020ae22adf4523827f6951f69689881
MaNeliaTerrell <nterrell1014@gmail.com>
2022-11-17 11:41:40 -0500

    Nelia's commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NTCard from './components/nt-component';
 
 function App() {
   return (
     <div className="App">
+      <NTCard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/nt-component/index.js
+++ src/components/nt-component/index.js
@@ -1,13 +1,22 @@
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 const NTCard = () => {
     return (
-        <div>
-            <h1>Nelia</h1>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://avatars.githubusercontent.com/u/110924630?s=60&v=4" />
+        <Card.Body>
+          <Card.Title>Nelia T</Card.Title>
+          <Card.Text>
+                Salt Life
+          </Card.Text>
+          {/* <Card.Link href="#">Github</Card.Link> */}
 
-        </div>
-    )
-}
+          <a href="https://github.com/MaNeliaTerrell" target='_blank' ><Button variant="primary">Github</Button></a>
+        </Card.Body>
+      </Card>
+    );
+};
 
 export default NTCard
